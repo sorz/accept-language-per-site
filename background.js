610a84172898e778f720a436435b5d9c1cbc6f00
sorz <orz@sorz.org>
2018-01-14 00:57:25 +0800

    Refactor: move handler & register to Rule class
    w/ help from @Jack-Works

--- background.js
+++ background.js
@@ -2,26 +2,12 @@
 
 let rules = [];
 
-function rewriteAcceptLanguage(e) {
-  let rule = rules[0];
-  for (let header of e.requestHeaders) {
-    if (header.name.toLowerCase() === "accept-language") {
-      header.value = rule.language;
-    }
-  }
-  return {requestHeaders: e.requestHeaders};
-}
-
 async function registerHandle() {
-  chrome.webRequest.onBeforeSendHeaders.removeListener(rewriteAcceptLanguage);
+  for (let rule of rules)
+    rule.unregister();
   rules = await getRules();
-  let patterns = rules.map(rule => rule.pattern);
-  if (!patterns) return;
-  chrome.webRequest.onBeforeSendHeaders.addListener(
-    rewriteAcceptLanguage,
-    {urls: patterns},
-    ["blocking", "requestHeaders"]
-  );
+  for (let rule of rules)
+    rule.register();
 }
 
 registerHandle().then(() => {
--- rules.js
+++ rules.js
@@ -1,11 +1,38 @@
+"use strict";
+
 class Rule {
   constructor(host, language) {
     this.host = host;
     this.language = language;
+    this.handleRewrite = this.handleRewrite.bind(this);
   }
 
   get pattern() {
     return `*://${this.host}/*`
+  }
+
+  handleRewrite(e) {
+    console.log("handle rewrite");
+    for (let header of e.requestHeaders) {
+      if (header.name.toLowerCase() === "accept-language") {
+        header.value = this.language;
+        console.log(`${e.host}: ${header.value}`);
+      }
+    }
+    return {requestHeaders: e.requestHeaders};
+  }
+
+  unregister() {
+    chrome.webRequest.onBeforeSendHeaders.removeListener(this.handleRewrite);
+  }
+
+  register(callback) {
+    console.log("add " + this.pattern);
+    chrome.webRequest.onBeforeSendHeaders.addListener(
+      this.handleRewrite,
+      {urls: [this.pattern]},
+      ["blocking", "requestHeaders"]
+    );
   }
 }
 
