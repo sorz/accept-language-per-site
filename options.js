a339336309eef501b324f012cfc9201d539e8b75
sorz <orz@sorz.org>
2018-01-26 17:40:16 +0800

    Add feedback for save

--- options.js
+++ options.js
@@ -1,30 +1,37 @@
 "use strict";
 
-async function saveOptions(e) {
-  e.preventDefault();
+const $ = selector => document.querySelector(selector);
+const sleep = ms => new Promise(cb => setTimeout(cb, ms));
+
+async function saveOptions(ev) {
+  ev.preventDefault();
+  ev.target.disabled = true;
+
   let list = document.querySelectorAll("#list li");
   let rules = Array.from(list).map(li => ({
       host: li.querySelector(".host").value,
       language: li.querySelector(".language").value
   })).filter(rule => rule.host && rule.language);
-  await browser.storage.local.set({
-    rules: rules
-  });
+  await browser.storage.local.set({ rules: rules });
+
+  ev.target.disabled = false;
+  $("#saved").classList.add("show");
+  await sleep(800);
+  $("#saved").classList.remove("show");
 }
 
 async function restoreOptions() {
   let rules = await getRules();
-  let list = document.querySelector("#list");
+  let list = $("#list");
   list.innerHTML = '';
   rules.forEach(rule => list.appendChild(rule.formHTML));
   list.appendChild(new Rule().formHTML);
 }
 
 function addMoreRule() {
-  document.querySelector("#list")
-    .appendChild(new Rule().formHTML);
+  $("#list").appendChild(new Rule().formHTML);
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
-document.querySelector("#more").addEventListener("click", addMoreRule);
+$("form").addEventListener("submit", saveOptions);
+$("#more").addEventListener("click", addMoreRule);
